43f5951728b94955424ac914b01bfda5c86ae6e8
saararbel <saararbel1@gmail.com>
2016-12-09 18:58:29 +0200

    perceptron algorithm added

--- perceptron_2d.py
+++ perceptron_2d.py
@@ -10,8 +10,26 @@ def parseDataSet(dataSetFilePath):
     return x,y
 
 
+def calcR(x):
+    R = 0
+    for idx,xi in enumerate(x):
+        lst = xi.tolist()
+        if(lst[0]*lst[0] + lst[1]*lst[1] > R):
+            R = lst[0]*lst[0] + lst[1]*lst[1]
 
-def perceptronAlgo(x, y):
+    return R
+
+def calcGama(x,y):
+    gama = sys.float_info.max
+    for idx,xi in enumerate(x):
+        lst = xi.tolist()
+        if(y[idx]*lst[0] + y[idx]*lst[1] < gama):
+            gama = y[idx]*lst[0] + y[idx]*lst[1]
+
+    return gama
+
+
+def perceptronAlgo(x, y, learningRate=1):
     w = [0] * 3
     input = []
     mistakes, iterations = 0,0
@@ -23,7 +41,7 @@ def perceptronAlgo(x, y):
             lst = xi.tolist()
             x1,x2 = lst[0] , lst[1]
             if ((w[0] * 1 + w[1] * x1 + w[2] * x2) * y[idx]) <= 0:
-                w = [y[idx]*1 + w[0], y[idx]*x1+ w[1],y[idx]*x2+ w[2]]
+                w = [y[idx]*1*learningRate + w[0], y[idx]*x1*learningRate + w[1],y[idx]*x2*learningRate + w[2]]
                 weightsChanged = False
                 mistakes += 1
 
@@ -32,13 +50,18 @@ def perceptronAlgo(x, y):
 
 if __name__ == '__main__':
     x,y = parseDataSet(sys.argv[1])
-    w,mistakes, iterations = perceptronAlgo(x,y)
+    w,mistakes, iterations = perceptronAlgo(x,y,0.3)
+    print w , mistakes , iterations
 
     outputFile = open("output.txt", 'w')
     outputFile.write("output1: " + str(w) + "\n")
     outputFile.write("output2: " + str(mistakes)+ "\n")
     outputFile.write("output3: " + str(iterations)+ "\n")
 
+    r = calcR(x)
+    gama = calcGama(x,y)
+
+    # print (r/gama)*(r/gama)
 
 
 
